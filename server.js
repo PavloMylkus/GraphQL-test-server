8a6a38c20609e963602878568d390848b13a0a08
PavloMylkus <ppavlo82@gmail.com>
2022-08-19 19:08:14 +0300

    Messages

--- server.js
+++ server.js
@@ -1,21 +1,80 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-	type Query {
-		hello: String
-		otherField: Int
-	}
+type Query {
+    getMessage(id: ID!): Message
+	messages: [Message]
+  }
+  
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
 `);
 
+
+class Message {
+	constructor(id, { content, author }) {
+		this.id = id;
+		this.content = content;
+		this.author = author;
+	}
+}
+
+const fakeDatabase = {}
+
 const rootValue = {
-	hello: 'Hello World',
-	otherField: 3
+	getMessage: ({ id }) => {
+		if (!fakeDatabase[id]) {
+			throw new Error('no message exists with id ' + id);
+		}
+		return new Message(id, fakeDatabase[id]);
+	},
+	createMessage: ({ input }) => {
+		var id = require('crypto').randomBytes(10).toString('hex');
+		fakeDatabase[id] = input;
+		return new Message(id, input);
+	},
+	updateMessage: ({ id, input }) => {
+		if (!fakeDatabase[id]) {
+			throw new Error('no message exists with id ' + id);
+		}
+		fakeDatabase[id] = input;
+		return new Message(id, input);
+	},
+	messages: () => {
+		// if (!Object.keys().length) {
+		// 	return null
+		// };
+
+		return Object.keys(fakeDatabase).map((key) => {
+			return new Message(key, fakeDatabase[key])
+		})
+	}
 };
 
-graphql({
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
 	schema,
 	rootValue,
-	source: '{hello, otherField}'
-}).then((response) => {
-	console.log(response);
-});
+	graphiql: true
+}))
+
+app.listen(4000);
+console.log('Hello!!')
